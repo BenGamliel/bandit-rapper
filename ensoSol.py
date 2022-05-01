e57567f6624a6fd974a3ac34e2d5d31574486fb2
BenGamliel <idov3r@gmail.com>
2022-05-01 21:50:23 +0300

    Add files via upload

--- ensoSol.py
+++ ensoSol.py
@@ -1,11 +1,10 @@
 
-
+#https://github.com/BenGamliel/enso.security-home-Assignment-
 import os
 import json
 import sys
 import linecache
 import hashlib
-
 
 def hash(toHash):
 	hash_object = hashlib.sha256()
@@ -27,7 +26,6 @@ def issueAttribute(i):
 		issueAttributes += codeExtract
 	return issueAttributes
 
-
 def calculateIssueHash(i):
 	return hash(issueAttribute(i))
 
@@ -36,14 +34,12 @@ def scanResult(issueFingerprint, i, issueSeverity):
 	YELC = '\033[33m'
 	BLUC = '\033[36m'
 	ENDC = '\033[0m'
-
 	if(issueSeverity=="HIGH"):
 		COLOR = REDC
 	elif(issueSeverity=="MEDIUM"):
 		COLOR = YELC
 	else:
 		COLOR = BLUC
-
 	output = "--------------------------------------------------\n"
 	output += COLOR + "Issue Fingerprint: " + issueFingerprint + "\n"
 	output += COLOR + "Issue Severity: %s \t Confidence Level: %s" % (i["issue_severity"], i["issue_confidence"]) + "\n"
@@ -58,25 +54,38 @@ def main(argv):
     path = sys.argv[1]
     stream = os.popen('bandit -r -f json ' + path)
     data = json.load(stream)
-    print('the len is \n{}\n'.format(len(data['results']))) # located in array
     findings = []
     for i in data['results']:
         findings.append(i)
-
     issue_weight = dict(HIGH=0, MEDIUM=1, LOW=2)
     findings.sort(key=lambda x: issue_weight[x["issue_confidence"]])
     findings.sort(key=lambda x: issue_weight[x["issue_severity"]])
-
     for i in findings:
         issueFingerprint = calculateIssueHash(i)
         print(scanResult(issueFingerprint, i, i["issue_severity"]))
-
 if __name__ == "__main__":
     main(sys.argv)
 
 
 
+#https://github.com/pyupio/safety-db
+#from bs4 import BeautifulSoup
+#def getCVEIds(soup, cveArray):
+#   table = soup.find('table', attrs={'class', 'searchresults'})
+#    for a in table.find_all('a', href=True):
+#        m = re.search("CVE-\d{4}-\d{4,7}", a['href'])
+#        if m:
+#            cveArray.append(m.group(0))
 
+
+#def getCVEPages(soup):
+#    cveIDPages = []
+#    items = soup.find_all('div', class_="paging")
+#    for item in items:
+#        links = item.find_all('a')
+#        for link in links:
+#            cveIDPages.append("http://www.cvedetails.com/" + str(link['href']))
+#    return cveIDPages
 
 
 
